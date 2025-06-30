a05b23eb8932ac908d221a9e857268b1918b85da
sivaguru004 <sivagurum004@gmail.com>
2025-06-30 21:03:57 +0530

    Commit
    initial commit

---
+++ base/urls.py
@@ -0,0 +1,8 @@
+# portfolio/base/urls.py
+from django.urls import path
+from .views import test, contact_form_submit
+
+urlpatterns = [
+    path('', test, name='test'),
+    path('contact/submit/', contact_form_submit, name='contact_form_submit'),
+]
